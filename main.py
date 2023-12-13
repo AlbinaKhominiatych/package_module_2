f13f70686138d8e4fa8da19a0037708ee64792cb
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-12-13 21:21:14 +0200

    можна зробити цікавішк

--- main.py
+++ main.py
@@ -1,23 +1,53 @@
-"""Серіалізація об'єктів класу у JSON:
-Створіть клас, який представляє об'єкт з деякими властивостями
-(наприклад, користувач з ім'ям та віком).
-Серіалізуйте екземпляр цього класу в JSON за
-допомогою json.dumps() та переконайтесь, що дані правильно серіалізовані."""
+"""Завдання 2
+Реалізація програми для додавання, видалення та
+відстеження завдань. Зберігати ці завдання у
+форматі JSON у файлі. Можливість завантаження
+раніше збережених завдань для подальшої роботи з
+ними."""
 import json
-class User:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-    def __str__(self):
-        return f"{self.name} {self.age}"
+import time
 
-user = User("Alice", 30)
-serialized_user = json.dumps(user.__dict__)
+task = {}
 
-print(serialized_user)
-load_user = json.loads(serialized_user)
-print(load_user)
-#user = User(load_user["name"], load_user["age"])
-#user = User(**load_user)
-user = User(*load_user.values())
-print(user)
+def load_task():
+    try:
+        with open("task.json", "r") as file:
+            task = json.load(file)
+    except FileNotFoundError:
+        task = {}
+    return task
+
+task = load_task()
+
+def save_task(task):
+    with open("task.json", "w") as file:
+        json.dump(task, file)
+
+def add_task(description):
+    task[time.ctime()] = description
+    save_task(task)
+
+def delete_task(key):
+    if key in task:
+        del task[key]
+        save_task(task)
+        print("замітка видалена")
+    else:
+        print("Немає такої замітки")
+
+menu = input()
+match menu:
+    case "1":
+        print("список завдань")
+        for key, value in load_task().items():
+            print(key, value)
+
+    case "2":
+        print("Введіть замітку")
+        description = input("Введіть опис завдання")
+        add_task(description)
+    case "3":
+        print("Введіть дату таску для видалення: ")
+        key = input()
+        #key = task.keys()[0]
+        delete_task(key)
